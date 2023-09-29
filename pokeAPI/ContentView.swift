9122d6b20004776b89f611104553b806d3226c10
AndrewA1537 <andrewanca1998@hotmail.com>
2023-09-29 15:19:32 -0700

    Initial Commit

---
+++ pokeAPI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  pokeAPI
+//
+//  Created by Petronela Anca on 2023-09-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ pokeAPI/pokeAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokeAPIApp.swift
+//  pokeAPI
+//
+//  Created by Petronela Anca on 2023-09-29.
+//
+
+import SwiftUI
+
+@main
+struct pokeAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
