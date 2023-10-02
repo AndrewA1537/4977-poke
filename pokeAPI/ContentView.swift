85cbf974b6d2ca82dbf60edb97d559cf89d54be5
AndrewA1537 <andrewanca1998@hotmail.com>
2023-10-02 10:32:49 -0700

    Test commit to ensure Github working.

--- pokeAPI/ContentView.swift
+++ pokeAPI/ContentView.swift
@@ -5,11 +5,18 @@
 //  Created by Petronela Anca on 2023-09-29.
 //
 
+
+
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
+
+
+struct ContentView: View
+{
+    var body: some View 
+    {
+        VStack 
+        {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
@@ -19,6 +26,9 @@ struct ContentView: View {
     }
 }
 
-#Preview {
+
+
+#Preview
+{
     ContentView()
 }
--- pokeAPI/pokeAPIApp.swift
+++ pokeAPI/pokeAPIApp.swift
@@ -5,12 +5,19 @@
 //  Created by Petronela Anca on 2023-09-29.
 //
 
+
+
 import SwiftUI
 
+
+
 @main
-struct pokeAPIApp: App {
-    var body: some Scene {
-        WindowGroup {
+struct pokeAPIApp: App 
+{
+    var body: some Scene
+    {
+        WindowGroup 
+        {
             ContentView()
         }
     }
